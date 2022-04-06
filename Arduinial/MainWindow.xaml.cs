b1df95e23bbfa4ebd9991650e848f4cf50cec372
FunnyMyth <52260596+FunnyMyth@users.noreply.github.com>
2022-04-06 15:49:56 -0500

    WPF "Hello, World"

--- Arduinial/MainWindow.xaml.cs
+++ Arduinial/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,26 @@ namespace Arduinial
         public MainWindow()
         {
             InitializeComponent();
+            TextBlock_ReadSerial.Text = "";
+            string[] ports = SerialPort.GetPortNames();
+            for(int i=0; i < ports.Length; i++)
+            {
+                ComboBox_Ports.Items.Add(ports[i]);
+            }
+            
+        }
+
+        //IDK what I'm doing, but here goes nothing
+        private void SerialMonitor()
+        {
+            string text = "Hello, World!\n";
+            //for(int i = 0; i < 100; i++) TextBox_SerialOutput.AppendText(text);
+            TextBlock_ReadSerial.Text = text;
+        }
+
+        private void Button_ReadSearial_Click(object sender, RoutedEventArgs e)
+        {
+            SerialMonitor();
         }
     }
 }
